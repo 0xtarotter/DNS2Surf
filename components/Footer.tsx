443796fdbb74d11efbd14902a7daa9c4e40fc7e0
TΛRØTTΞR <399929@gmail.com>
2024-12-16 19:39:09 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,6 +33,7 @@ export default function Footer() {
           {' '}
           {new Date().getFullYear()}
           {' '}
+          <mt/>
         </a>
         <span className="mt-4 inline-flex justify-center space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
 
