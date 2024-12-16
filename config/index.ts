a082b6fca34d9154032ebfbb752e16e7cbfcee2b
TΛRØTTΞR <399929@gmail.com>
2024-12-16 19:19:57 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,12 +2,12 @@ import { CLOUDFLARE_REGIONS } from './cloudflare'
 import { VERCEL_REGIONS } from './vercel'
 
 export const SITE = {
-  title: 'DNS.Surf',
+  title: 'DNS.Desi',
   tagline: 'Querying DNS Resolution Results in Different Regions Worldwide',
   description: 'Querying DNS Resolution Results in Different Regions Worldwide. 1. Supporting DNS Queries from Different Regions 2.Supporting 100+ DNS Resolvers',
-  keywords: 'DNS.Surf, DNS resolution, DNS query regions, global DNS resolver, Clouflare DNS, Vercel DNS, DNS exploration, DNS technology, DNS services',
-  image: 'https://dns.surf/banner.png',
-  url: 'https://dns.surf',
+  keywords: 'DNS.Desi, DNS resolution, DNS query regions, global DNS resolver, Clouflare DNS, Vercel DNS, DNS exploration, DNS technology, DNS services',
+  image: 'https://DNS.Desi/banner.png',
+  url: 'https://DNS.Desi',
 }
 
 export const REGIONS = process.env.NEXT_PUBLIC_CLOUDFLARE_WORKER_HOST ? CLOUDFLARE_REGIONS : VERCEL_REGIONS
